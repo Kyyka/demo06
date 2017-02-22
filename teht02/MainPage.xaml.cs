9a5faa9d85c3c46f8f7b8c8d666bcf6806d301f8
Eetu <Eetu@DESKTOP-FM7QUSI>
2017-02-22 20:36:22 +0200

    teht03 tehty

---
+++ teht02/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace teht02
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        //EuroTextBox empty by clicking
+        public void EuroTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= EuroTextBox_GotFocus;
+        }
+        //EuroTextBox filled with "Eurot" if clicked somewhere else
+        public void EuroTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (EuroTextBox.Text == "")
+            {
+                EuroTextBox.Text = "Eurot";
+            }
+        }
+
+        //MarkkaTextBox empty by clicking
+        public void MarkkaTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= MarkkaTextBox_GotFocus;
+        }
+        //MarkkaTextBox filled with "Markat" if clicked somewhere else
+        public void MarkkaTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (MarkkaTextBox.Text == "")
+            {
+                MarkkaTextBox.Text = "Markat";
+            }
+        }
+
+        private void EuroButton_Click(object sender, RoutedEventArgs e)
+        {
+            double euro = double.Parse(EuroTextBox.Text) * 6.0;
+            string euro_s = euro.ToString("0.00");
+            EuroTextBox.Text = euro_s;
+        }
+
+        private void MarkkaButton_Click(object sender, RoutedEventArgs e)
+        {
+            double markat = double.Parse(MarkkaTextBox.Text) / 6.0;
+            string markat_s = markat.ToString("0.00");
+            MarkkaTextBox.Text = markat_s;
+        }
+    }
+}
---
+++ teht03/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace teht03
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            int width = int.Parse(WidthTextBox.Text);
+            int height = int.Parse(HeightTextBox.Text);
+            int frame = int.Parse(FrameTextBox.Text);
+            
+            //WindowArea
+            int WindowArea = width * height / 10;
+            WindowAreaTextBox.Text = WindowArea.ToString() + " cm^2";
+
+            //GlassArea
+            int GlassArea = (width - frame * 2) * (height - frame * 2) / 10;
+            GlassAreaTextBox.Text = GlassArea.ToString() + " cm^2";
+
+            //FrameCircle
+            int FrameCircle = (width / 10 * 2) + (height / 10 * 2);
+            FrameCircleTextBox.Text = FrameCircle.ToString() + " cm";
+            
+        }
+    }
+}
